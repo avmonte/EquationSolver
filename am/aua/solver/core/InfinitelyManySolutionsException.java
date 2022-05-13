f30fb84274905388f6cb3fdc7b693a02bc8f71fb
avmonte <85803642+avmonte@users.noreply.github.com>
2022-05-13 23:28:24 +0400

    Add files via upload

---
+++ am/aua/solver/core/InfinitelyManySolutionsException.java
@@ -0,0 +1,13 @@
+package am.aua.solver.core;
+
+public class InfinitelyManySolutionsException extends Exception {
+
+    public InfinitelyManySolutionsException() {
+        super();
+    }
+
+    public InfinitelyManySolutionsException(String message) {
+        super(message);
+    }
+
+}
